91a09d575452e1e8aabdc3ffa04405f85195ad17
dodoman37 <168925242+dodoman37@users.noreply.github.com>
2024-05-15 18:50:14 +0200

    Add files via upload

---
+++ untitled1 2.py
@@ -0,0 +1,23 @@
+"""new code"""
+import nextcord
+import discord
+from datetime import datetime
+from discord.ext import commands
+from discord.ext.commands import MissingPermissions
+from nextcord.ext import commands
+
+bot = commands.Bot(command_prefix = '!')
+
+@bot.command(name="hi")
+async def SendMessage(ctx):
+    await ctx.send('Hello!')
+    
+@bot.event
+async def on_ready():
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}')
+    await bot.change_presence(activity=nextcord.CustomActivity(name="Hi harvici"),status='online')
+
+if __name__ == '__main__':
+    bot.run("MTIzOTk4MzAzOTg1ODIxMjkxNQ.G5mUrX.vByAXOM5A1AAN27NlXNltQPDqLKWBuO6WgjXCQ")
+
+"""new code"""
